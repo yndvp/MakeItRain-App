97b6da4650278ed49e535fc48aeaa26d2912b2aa
yndvp <58580623+yndvp@users.noreply.github.com>
2022-01-17 20:26:49 -0500

    add event listeners

--- app/src/main/java/com/example/makeitrain/MainActivity.java
+++ app/src/main/java/com/example/makeitrain/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.makeitrain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button makeItRain;
+    private Button showInfo;
+    private TextView moneyValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        makeItRain = findViewById(R.id.buttonMakeItRain);
+        moneyValue = findViewById(R.id.moneyValue);
+
+//        moneyValue.setText(R.string.test);
+
+        makeItRain.setOnClickListener(view -> Log.d("MainActivity", "onClick: Make it rain"));
+    }
+
+    public void showMoney(View view) {
+        Log.d("MainActivity", "onClick: Make it rain");
     }
 }
