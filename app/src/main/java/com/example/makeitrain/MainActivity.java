0e5e9fe797ce73cf8b292ca8668fee78a8be5db6
yndvp <58580623+yndvp@users.noreply.github.com>
2022-01-23 22:31:56 -0500

    add currency formatter /  add snackbar & toast

--- app/src/main/java/com/example/makeitrain/MainActivity.java
+++ app/src/main/java/com/example/makeitrain/MainActivity.java
@@ -7,11 +7,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
     private Button makeItRain;
     private Button showInfo;
     private TextView moneyValue;
+    private int moneyCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,32 @@ public class MainActivity extends AppCompatActivity {
 
 //        moneyValue.setText(R.string.test);
 
-        makeItRain.setOnClickListener(view -> Log.d("MainActivity", "onClick: Make it rain"));
+        // Attaching event listener
+//          makeItRain.setOnClickListener(new View.OnClickListener() {
+//              @Override
+//              public void onClick(View view) {
+//                  Log.d("MainActivity", "onClick: Make it rain");
+//              }
+//          });
+        // Attaching event listener: Lambda expression
+//        makeItRain.setOnClickListener(view -> Log.d("MainActivity", "onClick: Make it rain"));
     }
 
     public void showMoney(View view) {
-        Log.d("MainActivity", "onClick: Make it rain");
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+
+        moneyCounter += 1000;
+        moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
+        Log.d("MIR", "onClick: Make it rain " + moneyCounter);
+    }
+
+    public void showInfo(View view) {
+        Toast.makeText(MainActivity.this, R.string.app_info, Toast.LENGTH_SHORT)
+                .show();
+        Snackbar.make(moneyValue, R.string.app_info, Snackbar.LENGTH_LONG)
+                .setAction("More", view1 -> {
+                    Log.d("Snack", "showInfo: Snackbar More");
+                })
+                .show();
     }
 }
